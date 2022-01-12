1a1d8be962c4fdcf0ddf96a6de231f115d090df0
mathias labora martirena <mathiaslabora@gmail.com>
2022-01-12 08:56:51 -0300

    comienzo 12/1

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ app.use(express.urlencoded({ extended: false }));//se encarga de entender los da
 app.use(express.json());
 
 //conection db
+const { MongoClient } = require('mongodb');
+const password = 'quiz';
+const nameDB = 'quizSofka'
+const URI = `mongodb+srv://quiz:${password}@cluster0.9npo9.mongodb.net/${nameDB}?retryWrites=true&w=majority`
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = client.db(nameDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //init server
 app.listen(port, () =>
